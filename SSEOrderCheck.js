dd4d528c89dffceb456ae6ca5fced794a587ab9b
Coe <coe.gwathney@gmail.com>
2017-05-04 09:31:09 -0500

    Removed quotes from null

--- SSEOrderCheck.js
+++ SSEOrderCheck.js
@@ -11,11 +11,11 @@ function url() {
   } 
   var date = y + "-" + m + "-" + d; 
   var dealerNum = prompt("Please enter your dealer number.");
-  if(dealerNum === "null"){
+  if(dealerNum === null){
     return;
   }
   var dealerZip = prompt("Please enter your ZIP code."); 
-  if(dealerZip === "null"){
+  if(dealerZip === null){
     return;
   }
   return 'https://www.fedex.com/apps/fedextrack/?action=altref&trackingnumber='+ dealerNum +'&cntry_code=us&shipdate=' + date +'&account_number=&dest_cntry=us&dest_postal='+ dealerZip; 
