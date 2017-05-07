afb1c03ac3f8fbb404ba4b4f5aa612e9a45c1c3c
ccgthree <coe.gwathney@gmail.com>
2017-05-07 14:45:13 -0500

    Code Additions
    Added jquery, along with new callback functions, to improve link display
    on page.

--- SSEOrderCheckLinkTest.js
+++ SSEOrderCheckLinkTest.js
@@ -1,29 +1,58 @@
-function url() { 
-  var date = new Date(); 
-  var y = date.getFullYear(); 
-  var m = date.getMonth() +1; 
+function checkJq() {
+  if(!window.jQuery || !window.jQuery.ui) {
+    var jsDepend = [];
+    jsDepend.push('https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');
+    var pageHead = document.getElementsByTagName('head')[0];
+    var jqInject = document.createElement('script');
+    jqInject.setAttribute('type','text/javascript');
+    jqInject.setAttribute('src',jsDepend[0]);
+    pageHead.appendChild(jqInject);
+  } else {
+  }
+}
+
+checkJq();
+
+function url() {
+  var date = new Date();
+  var y = date.getFullYear();
+  var m = date.getMonth() +1;
   if(m < 10){
     m = '0' + m;
   } 
-  var d = date.getDate(); 
+  var d = date.getDate();
   if(d < 10){
     d = '0' + d;
   } 
-  var date = y + "-" + m + "-" + d; 
+  var shipDate = y + "-" + m + "-" + d;
   var dealerNum = prompt("Please enter your dealer number.");
   var dealerZip = prompt("Please enter your ZIP code.");
-  var linkResult = 'https://www.fedex.com/apps/fedextrack/?action=altref&trackingnumber='+ dealerNum +'&cntry_code=us&shipdate=' + date +'&account_number=&dest_cntry=us&dest_postal='+ dealerZip;
+  var linkResult = 'https://www.fedex.com/apps/fedextrack/?action=altref&trackingnumber='+ dealerNum +'&cntry_code=us&shipdate=' + shipDate +'&account_number=&dest_cntry=us&dest_postal='+ dealerZip;
   return linkResult;
 }
 
-function displayLink(){
-  var floatingPanel = document.creatElement('a');
-  var panelStyle = 'display:block;position:fixed;z-index:9999999;top:10px;width:200px;left:50%;margin-left:-100px;font-family:Helvetica;color:#ffffff;background-color:#f4a802;';
+function buildLink(callback){
+  var floatingPanel = document.createElement('a');
+  var panelStyle = 'display:none; position:fixed;z-index:9999999;top:10px;width:200px;left:50%;margin-left:-100px;font-family:Helvetica;color:#ffffff;background-color:#f4a802; text-decoration:none; text-align:center; box-shadow:0px 2px 10px #999999; border-radius:4px; ';
   floatingPanel.setAttribute('href', url());
   floatingPanel.setAttribute('style', panelStyle);
   floatingPanel.setAttribute('id', 'clickPanel');
   floatingPanel.setAttribute('target','_blank');
-  var linkText = document.createTextNode("Track Shipments");
-  floatingPanel.appendChild(linkText);
+  var linkHeader = document.createElement('h3');
+  linkHeader.setAttribute('style','line-height:40px; text-decoration:none; font-size-adjust:0.7;');
+  var linkText = document.createTextNode('Track Shipments');
+  floatingPanel.appendChild(linkHeader);
+  linkHeader.appendChild(linkText);
   document.body.appendChild(floatingPanel);
+  callback();
 }
+
+buildLink(function() {
+  setTimeout(function(){
+    $('#clickPanel').fadeIn(1000);
+    $('#clickPanel').click(function() {
+      $('#clickPanel').fadeOut(500);
+    });
+  }, 500);
+});
+
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,15 @@
 // Version 1 - No dynamic loading.
 javascript:function url() { var date = new Date(); var y = date.getFullYear(); var m = date.getMonth() +1; if(m < 10){m = '0' + m;} var d = date.getDate(); if(d < 10){d = '0' + d;} var date = y + "-" + m + "-" + d; var dealerNum = prompt("Please enter your dealer number."); var dealerZip = prompt("Please enter your ZIP code."); return 'https://www.fedex.com/apps/fedextrack/?action=altref&trackingnumber='+ dealerNum +'&cntry_code=us&shipdate=' + date +'&account_number=&dest_cntry=us&dest_postal='+ dealerZip; } window.open(url(),"_blank");
 
+// Version 2 - Dynamic Loading (EXPANDED VIEW)
+javascript:
+function(){
+	var jsCode = document.createElement('script');
+}
+jsCode.setAttribute('src','https://raw.githubusercontent.com/ccgthree/SSEOrderCheck/master/SSEOrderCheckLinkTest.js');
+document.body.appendChild(jsCode);
+jsCode.addEventListener('load', displayLink());
+
 
 // Version 3 - Dynamic Loading with load event listener.
 // this version is blocked by popup blockers because the user didn't expressly request
