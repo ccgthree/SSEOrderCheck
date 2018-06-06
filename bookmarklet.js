41678bc7a811edc810e9fe76c741b0b7f383265d
ccgthree <coe.gwathney@gmail.com>
2018-06-06 07:43:42 -0500

    Small revision to create a non-dynamic bookmarklet.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,12 +1,13 @@
-// Version 1 - No dynamic loading.
+// Version 0.1 - No dynamic loading.
 // This is the OG bookmarklet that I used.
 javascript:function url() { var date = new Date(); var y = date.getFullYear(); var m = date.getMonth() +1; if(m < 10){m = '0' + m;} var d = date.getDate(); if(d < 10){d = '0' + d;} var date = y + "-" + m + "-" + d; var dealerNum = prompt("Please enter your dealer number."); var dealerZip = prompt("Please enter your ZIP code."); return 'https://www.fedex.com/apps/fedextrack/?action=altref&trackingnumber='+ dealerNum +'&cntry_code=us&shipdate=' + date +'&account_number=&dest_cntry=us&dest_postal='+ dealerZip; } window.open(url(),"_blank");
 
-// Production-Ready Bookmarklet
-javascript:(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','https://rawgit.com/ccgthree/SSEOrderCheck/master/SSEOrderCheckLinkTest.js');document.body.appendChild(jsCode);doStuff();})();
+// Version 1 - Production-Ready Bookmarklet
+javascript:(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','https://rawgit.com/ccgthree/SSEOrderCheck/master/SSEOrderCheck.js');document.body.appendChild(jsCode);setTimeout(function(){doStuff();},500)})();
 
-/* HTML Code for Link
-
-<a id="codeOut" href="javascript:(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','https://rawgit.com/ccgthree/SSEOrderCheck/master/SSEOrderCheckLinkTest.js');document.body.appendChild(jsCode);doStuff();})();">Your Bookmarklet</a>
-
+/* HTML Markup for Link
+<a class="bookmarklet" href="javascript:(function(){var jsCode=document.createElement('script');jsCode.setAttribute('src','https://rawgit.com/ccgthree/SSEOrderCheck/master/SSEOrderCheck.js');document.body.appendChild(jsCode);setTimeout(function(){doStuff();},500)})();">SSE Tracking</a>
 */
+
+// Version 1.1 - My Bookmarklet
+javascript:function url() { var date = new Date(); var y = date.getFullYear(); var m = date.getMonth() +1; if(m < 10){m = '0' + m;} var d = date.getDate(); if(d < 10){d = '0' + d;} var date = y + "-" + m + "-" + d; var dealerNum = '46818'; var dealerZip = '35640'; return 'https://www.fedex.com/apps/fedextrack/?action=altref&trackingnumber='+ dealerNum +'&cntry_code=us&shipdate=' + date +'&account_number=&dest_cntry=us&dest_postal='+ dealerZip; } window.open(url(),"_blank");
